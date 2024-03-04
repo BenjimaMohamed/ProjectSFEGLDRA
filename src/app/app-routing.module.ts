070440a8528f362ecdfc2b2e9c08becb860978fa
Benjima <benjimamed@gmail.com>
2024-03-04 20:20:14 +0100

    Premier commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' }, // Redirection par défaut vers le composant de connexion
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
