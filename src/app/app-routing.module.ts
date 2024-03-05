0781ab9cddf804e9b34948903e9e105f2526ab48
Benjima <benjimamed@gmail.com>
2024-03-05 21:45:33 +0100

    1ere phase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ContactComponent } from './contact/contact.component';
+import { ActualiteComponent } from './actualite/actualite.component';
+import { SuccessComponent } from './success/success.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/accueil', pathMatch: 'full' }, // Redirection par défaut vers le composant de connexion
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' }, 
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'carte', component: CategoriesComponent },
+  { path: 'acualite', component: ActualiteComponent },
+  { path: 'success', component: SuccessComponent },
+
+
+
+
+
+
+
+  // Redirection par défaut vers le composant de connexion
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CarteComponent } from './carte/carte.component';
+import { SuccessComponent } from './success/success.component';
+import { ActualiteComponent } from './actualite/actualite.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccueilComponent
+    AccueilComponent,
+    CategoriesComponent,
+    CarteComponent,
+    SuccessComponent,
+    ActualiteComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
